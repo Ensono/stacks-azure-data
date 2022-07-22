35223dff80da85d84bb1af88841c9ac4200d5094
Zaid Khider <108268172+zaidkhider1998@users.noreply.github.com>
2022-07-22 15:12:02 +0100

    Update main.tf

--- deploy/azure/infra/main.tf
+++ deploy/azure/infra/main.tf
@@ -9,24 +9,18 @@
    tags       = var.tags
  }
 
- module "adf" {
-   source = "git::https://github.com/amido/stacks-terraform//azurerm/modules/azurerm-data/adf?ref=v0.1.0"
-   region                                        = var.region
+ module "app" {
+   source = "git::https://github.com/amido/stacks-terraform//azurerm/modules/azurerm-data?ref=v0.1.0"
+   resource_namer                                = module.default_label.id
+   resource_tags                                 = module.default_label.tags
+   resource_group_location                       = var.resource_group_location 
    data_factory_name                             = var.data_factory_name
-   data_platform_log_analytics_workspace_id      = var.data_platform_log_analytics_workspace_id
-   platform_scope                                = var.platform_scope
-   resource_group_name                           = azurerm_resource_group.default.name
+   core_resource_group                           = data.terraform_remote_state.core.outputs.resource_group_name
    adls_storage_account_id                       = module.adls.adls_storage_account_id
    default_storage_account_id                    = module.adls.default_storage_account_id
    adls_storage_account_primary_dfs_endpoint     = module.adls.adls_storage_account_primary_dfs_endpoint
    default_storage_account_primary_blob_endpoint = module.adls.default_storage_account_primary_blob_endpoint
- }
-
- module "adls" {
-   source = "git::https://github.com/amido/stacks-terraform//azurerm/modules/azurerm-data/adls?ref=v0.1.0"
-   adls_storage_account_name    = var.adls_storage_account_name
-   default_storage_account_name = var.default_storage_account_name
-   platform_scope               = var.platform_scope
-   region                       = var.region
-   resource_group_name          = azurerm_resource_group.default.name
+   adls_storage_account_name                     = var.adls_storage_account_name
+   default_storage_account_name                  = var.default_storage_account_name
+   subscription_id                               = data.azurerm_client_config.current.subscription_id
  }
