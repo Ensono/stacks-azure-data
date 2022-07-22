223d4ed47c5dc3ec30137db8ab764b8b0d26014c
Zaid Khider <108268172+zaidkhider1998@users.noreply.github.com>
2022-07-22 15:17:44 +0100

    Update variables.tf

--- deploy/azure/infra/variables.tf
+++ deploy/azure/infra/variables.tf
@@ -65,16 +65,6 @@ variable "data_factory_name" {
   description = "Data Factory Name"
 }
 
-variable "region" {
-  type        = string
-  description = "Region"
-}
-
-variable "resource_group_name" {
-  type        = string
-  description = "Resource Group Name"
-}
-
 variable "adls_storage_account_name" {
   type        = string
   description = "ADLS Storage Account Name"
@@ -85,40 +75,6 @@ variable "default_storage_account_name" {
   description = "Default Storage Account Name"
 }
 
-variable "platform_scope" {
-  type        = string
-  description = "Platform Scope"
-}
-
-variable "adls_account_replication_type" {
-  type        = string
-  description = "The ADLS Storage Account replication type"
-  default     = "LRS"
-}
-
-variable "adls_containers" {
-  type        = set(string)
-  description = "ADLS containers to create"
-  default     = ["dev"]
-}
-
-variable "application_insights_daily_data_cap_in_gb" {
-  type        = number
-  description = "The daily cap in ingesting data, once reached no more data will be ingested for the period."
-  default     = 5
-}
-
-variable "application_insights_daily_data_cap_notifications_disabled" {
-  type        = bool
-  description = "Enables or disables the daily cap notification."
-  default     = true
-}
-
-variable "application_insights_retention_in_days" {
-  type        = number
-  description = "Number of days data is retained for - this only applies to the classic resource."
-  default     = 30
-}
 ###########################
 # Core infrastructure settings
 ##########################
@@ -131,7 +87,7 @@ variable "core_environment" {
 variable "tfstate_key" {
   type        = string
   description = "Name of the key in remote storage for the core environmnent"
-  default     = "core-sharedservicesenv"
+  default     = "stacks-data-core"
 }
 
 variable "tfstate_storage_account" {
