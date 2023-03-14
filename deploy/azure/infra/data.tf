a46bdc0cf0f8cd05b55f29aec1fca1b2e5520297
“Rishikesh <rishikesh_singh@next.co.uk>
2023-03-14 12:25:40 +0000

    feat: data privioning

---
+++ deploy/azure/infra/data.tf
@@ -0,0 +1,13 @@
+data "azurerm_storage_account" "linked_blob" {
+  name                = module.adls_default.storage_account_names[0]
+  resource_group_name = azurerm_resource_group.default.name
+}
+
+
+data "azurerm_storage_account" "linked_blob_config" {
+  name                = module.adls_default.storage_account_names[1]
+  resource_group_name = azurerm_resource_group.default.name
+}
+
+
+data "azurerm_client_config" "current" {}
