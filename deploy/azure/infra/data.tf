8015e1c443d49002c8818a454f1019436cf98adf
“Rishikesh <rishikesh_singh@next.co.uk>
2023-03-14 14:31:58 +0000

    fixed: updated storage kind

--- deploy/azure/infra/data.tf
+++ deploy/azure/infra/data.tf
@@ -1,11 +1,11 @@
 data "azurerm_storage_account" "linked_adls" {
-  name                = module.adls_default.storage_account_names[0]
+  name                = module.adls_default.storage_account_names[1]
   resource_group_name = azurerm_resource_group.default.name
 }
 
 
 data "azurerm_storage_account" "linked_blob_config" {
-  name                = module.adls_default.storage_account_names[1]
+  name                = module.adls_default.storage_account_names[0]
   resource_group_name = azurerm_resource_group.default.name
 }
 
