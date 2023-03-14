ae1afc37f12f820b0d934ea14df777af5c7c3620
“Rishikesh <rishikesh_singh@next.co.uk>
2023-03-14 14:16:02 +0000

    fixed: adls

--- deploy/azure/infra/data.tf
+++ deploy/azure/infra/data.tf
@@ -1,4 +1,4 @@
-data "azurerm_storage_account" "linked_blob" {
+data "azurerm_storage_account" "linked_adls" {
   name                = module.adls_default.storage_account_names[0]
   resource_group_name = azurerm_resource_group.default.name
 }
