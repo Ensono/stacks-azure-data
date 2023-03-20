37919e03283872e0b665ab879b326cb29e7bbf3c
adurkan-amido <andy.durkan@amido.com>
2023-03-20 15:38:21 +0000

    Restructure repo for Azure ingest only

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ install_dev_environment:
 	echo "PYTHONPATH=." > .env
 	poetry self add poetry-dotenv-plugin
 	poetry shell
+	poetry update
 
 test:
 	pytest tests/unit/
--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -4,7 +4,7 @@ from utils.config_utils import load_config_as_dict, load_configs_as_list
 
 
 def test_config_valid_ingest_sources():
-    ingest_sources_dir = "ingest-sources"
+    ingest_sources_dir = "ingest_sources"
     schema = load_config_as_dict(
         f"{ingest_sources_dir}/schema/ingest_config_schema.json"
     )
