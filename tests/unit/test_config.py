22173e319632df86bd01eb67c50592cb4bb39c18
adurkan-amido <andy.durkan@amido.com>
2023-03-21 16:55:54 +0000

    Update ingest config and tests

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -1,12 +1,26 @@
+from collections import Counter
 from jsonschema import validate
 
 from utils.config_utils import load_config_as_dict, load_configs_as_list
+from utils.constants import INGEST_SOURCES_DIR, INGEST_SOURCES_SCHEMA_PATH
 
 
-def test_config_valid_ingest_sources():
-    ingest_sources_dir = "ingest_sources"
-    schema = load_config_as_dict(
-        f"{ingest_sources_dir}/schema/ingest_config_schema.json"
-    )
-    for config in load_configs_as_list(ingest_sources_dir):
+def test_config_ingest_sources_schema_valid():
+    schema = load_config_as_dict(INGEST_SOURCES_SCHEMA_PATH)
+    all_configs = load_configs_as_list(INGEST_SOURCES_DIR)
+    for config in all_configs:
         validate(config, schema)
+
+
+def test_config_ingest_sources_uniqueness():
+    all_configs = load_configs_as_list(INGEST_SOURCES_DIR)
+
+    # Data sources unique
+    data_source_counts = Counter(i["data_source_name"] for i in all_configs)
+    for config in all_configs:
+        assert data_source_counts[config["data_source_name"]] == 1
+
+        # Ingest entities unique per source
+        entity_counts = Counter(i["display_name"] for i in config["ingest_entities"])
+        for entity in config["ingest_entities"]:
+            assert entity_counts[entity["display_name"]] == 1
---
+++ utils/constants.py
@@ -0,0 +1,7 @@
+"""
+Constants used throughout the project
+"""
+
+# Config files
+INGEST_SOURCES_DIR = "ingest_sources"
+INGEST_SOURCES_SCHEMA_PATH = f"{INGEST_SOURCES_DIR}/schema/ingest_config_schema.json"
