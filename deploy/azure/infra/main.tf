1772fbd4b228ae8e4975b03676f280e16902d9a6
satenderrathee <88006547+satenderrathee@users.noreply.github.com>
2023-04-18 15:36:07 +0100

    Feature/adb (#26)
    * added databricks workspace
    
    * updated
    
    * fixed
    
    * fixed
    
    * added diagnostic setting for ADB
    
    * updated for databricks
    
    * updated format
    
    * disable_databricksws_diagnostic test
    
    * enabled_databricksws_diagnostic
    
    * removed log_analytics_destination_type
    
    * disable_databricksws_diagnostic
    
    * enabled_databricksws_diagnostic
    
    ---------
    
    Co-authored-by: “Rishikesh <rishikesh_singh@next.co.uk>

--- deploy/azure/infra/main.tf
+++ deploy/azure/infra/main.tf
@@ -140,10 +140,10 @@ resource "azurerm_key_vault_secret" "secrets" {
 }
 
 resource "azurerm_key_vault_secret" "sql_password" {
-  name = var.sql_password
-  value = module.sql.sql_sa_password
+  name         = var.sql_password
+  value        = module.sql.sql_sa_password
   key_vault_id = module.kv_default.id
-  }
+}
 
 
 # Storage accounts for data lake and config
@@ -159,8 +159,28 @@ module "sql" {
 }
 
 resource "azurerm_key_vault_secret" "sql_connect_string" {
-  for_each = toset(var.sql_db_names)
-  name = "connect-string-${each.key}"
-  value = "Server=tcp:${module.sql.sql_server_name}.database.windows.net,1433;Database=${each.key};User ID=${module.sql.sql_sa_login};Password=${module.sql.sql_sa_password};Trusted_Connection=False;Encrypt=True;Connection Timeout=30"
+  for_each     = toset(var.sql_db_names)
+  name         = "connect-string-${each.key}"
+  value        = "Server=tcp:${module.sql.sql_server_name}.database.windows.net,1433;Database=${each.key};User ID=${module.sql.sql_sa_login};Password=${module.sql.sql_sa_password};Trusted_Connection=False;Encrypt=True;Connection Timeout=30"
   key_vault_id = module.kv_default.id
-  }
+}
+
+# databricks workspace
+module "adb" {
+  source                                   = "git::https://github.com/amido/stacks-terraform//azurerm/modules/azurerm-adb?ref=feature/azuredatabricks"
+  resource_namer                           = module.default_label.id
+  resource_group_name                      = azurerm_resource_group.default.name
+  resource_group_location                  = azurerm_resource_group.default.location
+  databricks_sku                           = var.databricks_sku
+  resource_tags                            = module.default_label.tags
+  enable_databricksws_diagnostic           = var.enable_databricksws_diagnostic
+  data_platform_log_analytics_workspace_id = azurerm_log_analytics_workspace.la.id
+  databricksws_diagnostic_setting_name     = var.databricksws_diagnostic_setting_name
+
+}
+
+resource "azurerm_role_assignment" "adb_role" {
+  scope                = module.adb.adb_databricks_id
+  role_definition_name = "Contributor"
+  principal_id         = module.adf.adf_managed_identity
+}
--- deploy/azure/infra/vars.tf
+++ deploy/azure/infra/vars.tf
@@ -209,3 +209,26 @@ variable "sql_password" {
   default     = "sql-password"
   description = "Name of the Key for Sql admin Password, its not the actual value/password but the by the name its referred to."
 }
+
+variable "databricks_sku" {
+  type        = string
+  default     = "premium"
+  description = "The SKU to use for the databricks instance"
+
+  validation {
+    condition     = can(regex("standard|premium|trial", var.databricks_sku))
+    error_message = "Err: Valid options are 'standard', 'premium' or 'trial'."
+  }
+}
+
+variable "enable_databricksws_diagnostic" {
+  type        = bool
+  description = "Whether to enable diagnostic settings for the Azure Databricks workspace"
+  default     = true
+}
+
+variable "databricksws_diagnostic_setting_name" {
+  type        = string
+  default     = "Databricks to Log Analytics"
+  description = "The Databricks workspace diagnostic setting name."
+}
