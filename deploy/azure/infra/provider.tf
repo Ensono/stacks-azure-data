1d291e7019ef397a3ec31f31cb8a7afee4e94dd1
Satenderrathee <satender.rathee07@gmail.com>
2023-04-21 14:38:04 +0100

    updated provider

--- deploy/azure/infra/provider.tf
+++ deploy/azure/infra/provider.tf
@@ -8,7 +8,9 @@ provider "azurerm" {
 }
 
 provider "databricks" {
-  host = module.adb.databricks_hosturl
+  host                        = module.adb.databricks_hosturl
+  azure_workspace_resource_id = module.adb.adb_databricks_id
+
   # ARM_USE_MSI environment variable is recommended
   azure_use_msi = true
 
