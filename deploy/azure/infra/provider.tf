066dd468821500e007e33653613a1ea4e63c069d
Satenderrathee <satender.rathee07@gmail.com>
2023-04-21 14:45:06 +0100

    fixed

--- deploy/azure/infra/provider.tf
+++ deploy/azure/infra/provider.tf
@@ -11,7 +11,4 @@ provider "databricks" {
   host                        = module.adb.databricks_hosturl
   azure_workspace_resource_id = module.adb.adb_databricks_id
 
-  # ARM_USE_MSI environment variable is recommended
-  azure_use_msi = true
-
 }
