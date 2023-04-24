adc80467dd29c18119608a73a5a3652267f738c1
Satenderrathee <satender.rathee07@gmail.com>
2023-04-24 13:29:18 +0100

    updated provider

--- deploy/azure/infra/provider.tf
+++ deploy/azure/infra/provider.tf
@@ -10,8 +10,8 @@ provider "azurerm" {
 provider "databricks" {
   host                        = module.adb.databricks_hosturl
   azure_workspace_resource_id = module.adb.adb_databricks_id
-  azure_client_id = "${ARM_CLIENT_ID}"
-  azure_client_secret = "${ARM_CLIENT_SECRET}"
-  azure_tenant_id = "${ARM_TENANT_ID}"
+  azure_client_id            = var.ARM_CLIENT_ID
+  azure_client_secret         = var.ARM_CLIENT_SECRET
+  azure_tenant_id             = var.ARM_TENANT_ID
 
 }
