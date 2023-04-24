fc4ba3dd6c2b339acac73cb60acfbddc766e37c4
Satenderrathee <satender.rathee07@gmail.com>
2023-04-24 16:31:11 +0100

    updated

--- deploy/azure/infra/provider.tf
+++ deploy/azure/infra/provider.tf
@@ -10,8 +10,8 @@ provider "azurerm" {
 provider "databricks" {
   host                        = module.adb.databricks_hosturl
   azure_workspace_resource_id = module.adb.adb_databricks_id
- # azure_client_id            = var.ARM_CLIENT_ID
- # azure_client_secret         = var.ARM_CLIENT_SECRET
- # azure_tenant_id             = var.ARM_TENANT_ID
+  azure_client_id            = env("AZURE_CLIENT_ID")
+  azure_client_secret = env("AZURE_CLIENT_SECRET")
+  azure_tenant_id     = env("AZURE_TENANT_ID")
 
 }
