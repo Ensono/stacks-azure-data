dd09197534f31a036ac628b6b2dec338e32c4754
George Calvert <george.calvert@amido.com>
2023-04-24 10:52:37 +0100

    Add and use fixtures plus some refactoring

--- tests/end_to_end/features/azure_data_ingest.feature
+++ tests/end_to_end/features/azure_data_ingest.feature
@@ -2,9 +2,10 @@ Feature:Azure Data Ingest
   I want to ingest data
   so that it is available in Azure data lake storage
 
-  Scenario Outline: Test Scenario
+  Scenario Outline: Data Factory Ingest SQL Database into ADLS
     Given the ADF pipeline Ingest_AzureSql_Example has been triggered
     And the ADF pipeline Ingest_AzureSql_Example has finished with state Succeeded
+    And the ADF pipeline completed in less than 120 seconds
     Then the parquet files <output_files> are present in the ADLS container raw in the directory example_azuresql_1
 
     Examples: Output files
--- tests/end_to_end/features/environment.py
+++ tests/end_to_end/features/environment.py
@@ -1,19 +1,6 @@
-import os
-from azure.identity import DefaultAzureCredential
-from azure.storage.filedatalake import DataLakeDirectoryClient
-from constants import ADLS_URL, SQL_DB_INGEST_DIRECTORY_NAME, RAW_CONTAINER_NAME
-from azure.core.exceptions import ResourceNotFoundError
+from fixtures import azure_adls_clean_up
+from behave import use_fixture
 
 
 def before_scenario(context, scenario):
-    credential = DefaultAzureCredential()
-    client = DataLakeDirectoryClient(account_url=ADLS_URL, credential=credential, file_system_name=RAW_CONTAINER_NAME,
-                                     directory_name=SQL_DB_INGEST_DIRECTORY_NAME)
-    print(f"BEFORE SCENARIO. DELETING DIRECTORY: {ADLS_URL}/{RAW_CONTAINER_NAME}/{SQL_DB_INGEST_DIRECTORY_NAME}")
-
-    try:
-        client.delete_directory()
-    except ResourceNotFoundError:
-        print(f"The Following Directory Was Not Found: {ADLS_URL}/{RAW_CONTAINER_NAME}/{SQL_DB_INGEST_DIRECTORY_NAME}")
-    except Exception as e:
-        raise
+    use_fixture(azure_adls_clean_up, context)
