fee83e61472d4ba0fe50ba008bfb28fa4bd2c6c3
Satenderrathee <satender.rathee07@gmail.com>
2023-04-24 13:05:22 +0100

    updated provider

--- deploy/azure/infra/provider.tf
+++ deploy/azure/infra/provider.tf
@@ -3,10 +3,6 @@ terraform {
   }
 }
 
-default_provider {
-  name = "azurerm"
-}
-
 provider "azurerm" {
   features {}
 }
@@ -14,5 +10,8 @@ provider "azurerm" {
 provider "databricks" {
   host                        = module.adb.databricks_hosturl
   azure_workspace_resource_id = module.adb.adb_databricks_id
+  azure_client_id = "${var.ARM_CLIENT_ID}"
+  azure_client_secret = "${var.ARM_CLIENT_SECRET}"
+  azure_tenant_id = "${var.ARM_TENANT_ID}"
 
 }
