78e9d9c160d36a9f0eccc32d5a17b82fb428fe09
George Calvert <george.calvert@amido.com>
2023-04-27 16:54:26 +0100

    More refactoring

--- tests/end_to_end/features/fixtures.py
+++ tests/end_to_end/features/fixtures.py
@@ -4,25 +4,52 @@ from constants import ADLS_URL, RAW_CONTAINER_NAME, AUTOMATED_TEST_OUTPUT_DIRECT
 from behave import fixture
 
 
-def filter_fs_paths_adls(adls_client: DataLakeServiceClient, directory_name: str,
-                         sub_directory_prefix: str) -> list:
-    adls_fs_client = adls_client.get_file_system_client(RAW_CONTAINER_NAME)
+def filter_directory_paths_adls(adls_client: DataLakeServiceClient, container_name: str, directory_path: str,
+                                directory_substring: str) -> list:
+    """
+    Filters an ADLS container directory for directories containing a given substring.
+
+    :param adls_client:               DataLakeServiceClient
+    :param container_name:            Container / file system
+    :param directory_path:            Directory
+    :param directory_substring:      String to be found in directory
+    :return:
+        list of directory paths containing the sub_directory prefix
+    """
+    adls_fs_client = adls_client.get_file_system_client(container_name)
     output_directory_paths = []
-    if adls_fs_client.get_directory_client(directory_name).exists():
-        paths = adls_fs_client.get_paths(directory_name)
+    if adls_fs_client.get_directory_client(directory_path).exists():
+        paths = adls_fs_client.get_paths(directory_path)
         for path in paths:
-            if path.is_directory and sub_directory_prefix in path.name:
+            if path.is_directory and directory_substring in path.name:
                 output_directory_paths.append(path.name)
     return output_directory_paths
 
 
 def delete_directories_adls(adls_client: DataLakeServiceClient, container_name: str, directory_paths: list):
+    """
+    Deletes a list of directories from ADLS.
+
+    :param adls_client:     DataLakeServiceClient
+    :param container_name:  Container / file system
+    :param directory_paths: list of directories to delete
+    :return:
+        None
+    """
     for directory_path in directory_paths:
         print(f"ATTEMPTING TO DELETE DIRECTORY: {directory_path}")
         delete_directory_adls(adls_client, container_name, directory_path)
 
 
 def delete_directory_adls(adls_client: DataLakeServiceClient, container_name, directory_path: str):
+    """
+    Deletes an ADLS directory.
+    :param adls_client:     DataLakeServiceClient
+    :param container_name:  Container / File System
+    :param directory_path:  A directory path
+    :return:
+        None
+    """
     adls_directory_client = adls_client.get_directory_client(container_name, directory_path)
     if adls_directory_client.exists():
         adls_directory_client.delete_directory()
@@ -37,9 +64,10 @@ def azure_adls_clean_up(context, ingest_directory_name: str):
 
     print('BEFORE SCENARIO. DELETING ANY AUTOMATED TEST OUTPUT DATA')
 
-    automated_test_output_directory_paths = filter_fs_paths_adls(adls_client,
-                                                                 ingest_directory_name,
-                                                                 AUTOMATED_TEST_OUTPUT_DIRECTORY_PREFIX)
+    automated_test_output_directory_paths = filter_directory_paths_adls(adls_client,
+                                                                        RAW_CONTAINER_NAME,
+                                                                        ingest_directory_name,
+                                                                        AUTOMATED_TEST_OUTPUT_DIRECTORY_PREFIX)
 
     if automated_test_output_directory_paths:
         delete_directories_adls(adls_client, RAW_CONTAINER_NAME, automated_test_output_directory_paths)
@@ -48,9 +76,10 @@ def azure_adls_clean_up(context, ingest_directory_name: str):
 
     print('AFTER SCENARIO. DELETING ANY AUTOMATED TEST OUTPUT DATA')
 
-    automated_test_output_directory_paths = filter_fs_paths_adls(adls_client,
-                                                                 ingest_directory_name,
-                                                                 AUTOMATED_TEST_OUTPUT_DIRECTORY_PREFIX)
+    automated_test_output_directory_paths = filter_directory_paths_adls(adls_client,
+                                                                        RAW_CONTAINER_NAME,
+                                                                        ingest_directory_name,
+                                                                        AUTOMATED_TEST_OUTPUT_DIRECTORY_PREFIX)
 
     if automated_test_output_directory_paths:
         delete_directories_adls(adls_client, RAW_CONTAINER_NAME, automated_test_output_directory_paths)
