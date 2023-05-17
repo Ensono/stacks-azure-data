2723eed2878e25b3a1a24c92efc2935ff413905c
Lorraine Snaddon <lorraine.snaddon@amido.com>
2023-05-17 14:45:10 +0100

    Revert "Fix tests"
    This reverts commit 83f0b1659be7edd3b11361a0f2f047daa02fc9e4.

--- ingest/jobs/Get_Ingest_Config/tests/end_to_end/features/steps/azure_data_ingest_steps.py
+++ ingest/jobs/Get_Ingest_Config/tests/end_to_end/features/steps/azure_data_ingest_steps.py
@@ -3,5 +3,6 @@ from utils.test.e2e.shared_steps import (
     poll_adf_pipeline,
     pipeline_has_finished_with_state,
     check_all_files_present_in_adls,
-    check_adf_pipeline_completion_time
+    check_adf_pipeline_completion_time,
+    check_config_files_present_in_adls
 )
