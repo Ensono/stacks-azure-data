d58cf7d3860aac078d0968e268bacfd2496e7785
“Rishikesh <rishikesh_singh@next.co.uk>
2023-05-26 11:08:06 +0100

    fixed

--- deploy/azure/infra/main.tf
+++ deploy/azure/infra/main.tf
@@ -185,20 +185,20 @@ resource "azurerm_key_vault_secret" "sql_password" {
 
 # Storage accounts for data lake and config
 module "sql" {
-  source                          = "git::https://github.com/amido/stacks-terraform//azurerm/modules/azurerm-sql?ref=feat-add-pe-sql"
-  resource_namer                  = module.default_label.id
-  resource_group_name             = azurerm_resource_group.default.name
-  resource_group_location         = azurerm_resource_group.default.location
-  sql_version                     = var.sql_version
-  administrator_login             = var.administrator_login
-  sql_db_names                    = var.sql_db_names
-  resource_tags                   = module.default_label.tags
-  managed_virtual_network_enabled = var.managed_virtual_network_enabled
-  pe_subnet_id                    = data.azurerm_subnet.pe_subnet.id
-  pe_resource_group_name          = data.azurerm_subnet.pe_subnet.resource_group_name
-  pe_resource_group_location      = var.pe_resource_group_location
-  private_dns_zone_name           = data.azurerm_private_dns_zone.private_dns.name
-  private_dns_zone_ids            = ["${data.azurerm_private_dns_zone.private_dns.id}"]
+  source                     = "git::https://github.com/amido/stacks-terraform//azurerm/modules/azurerm-sql?ref=feat-add-pe-sql"
+  resource_namer             = module.default_label.id
+  resource_group_name        = azurerm_resource_group.default.name
+  resource_group_location    = azurerm_resource_group.default.location
+  sql_version                = var.sql_version
+  administrator_login        = var.administrator_login
+  sql_db_names               = var.sql_db_names
+  resource_tags              = module.default_label.tags
+  enable_private_network     = var.enable_private_network
+  pe_subnet_id               = data.azurerm_subnet.pe_subnet.id
+  pe_resource_group_name     = data.azurerm_subnet.pe_subnet.resource_group_name
+  pe_resource_group_location = var.pe_resource_group_location
+  private_dns_zone_name      = data.azurerm_private_dns_zone.private_dns.name
+  private_dns_zone_ids       = ["${data.azurerm_private_dns_zone.private_dns.id}"]
 
 }
 
