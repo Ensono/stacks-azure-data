45f74107635a876d468428b582b922b9b59db15a
Paulina Motyl <paulina.motyl@ensono.com>
2023-06-02 10:51:14 +0200

    feat: 6185 consts refactor

--- pysparkle/pysparkle/silver.py
+++ pysparkle/pysparkle/silver.py
@@ -3,8 +3,10 @@ import logging
 from pathlib import Path
 
 from pyspark.sql import SparkSession
-from pysparkle.adls_utils import get_directory_contents, set_env, set_spark_properties
-from pysparkle.const import ADLS_ACCOUNT, BRONZE_CONTAINER, DATASET_NAME, SILVER_CONTAINER
+
+from pysparkle.adls_utils import check_env, get_adls_file_url, get_directory_contents, \
+    set_spark_properties
+from pysparkle.config import BRONZE_CONTAINER, SILVER_CONTAINER
 
 logger = logging.getLogger(__name__)
 
@@ -19,20 +21,6 @@ def filter_csv_files(paths: list[str]) -> list[str]:
         A list of file paths that end with the `.csv` extension.
     """
     return [path for path in paths if path.endswith('.csv')]
-
-
-def get_adls_url(container: str, adls_account: str, file_name: str) -> str:
-    """Constructs an Azure Data Lake Storage (ADLS) URL for a specific file.
-
-    Args:
-        container: The name of the ADLS container.
-        adls_account: The name of the ADLS account.
-        file_name: The name of the file (including any subdirectories within the container).
-
-    Returns:
-        Full ADLS URL for the specified file.
-    """
-    return f'abfss://{container}@{adls_account}.dfs.core.windows.net/{file_name}'
 
 
 def ensure_database_exists(spark: SparkSession, schema: str) -> None:
@@ -58,7 +46,7 @@ def save_files_as_delta_tables(spark: SparkSession, csv_files: list[str]) -> Non
         csv_files: List of CSV files to be converted into Delta tables.
     """
     def to_delta(csv_file: str) -> None:
-        filepath = get_adls_url(BRONZE_CONTAINER, ADLS_ACCOUNT, csv_file)
+        filepath = get_adls_file_url(BRONZE_CONTAINER, csv_file)
         filename_with_no_extension = Path(filepath).stem
         df = spark.read.option('header', 'true')\
             .option('inferSchema', 'true')\
@@ -74,16 +62,16 @@ def save_files_as_delta_tables(spark: SparkSession, csv_files: list[str]) -> Non
         to_delta(file)
 
 
-def silver_main():
+def silver_main(dataset_name):
     logger.info('Running Silver processing...')
     spark = SparkSession \
         .builder \
         .appName('Silver') \
         .getOrCreate()
 
-    set_env()
+    check_env()
     set_spark_properties(spark)
-    input_paths = get_directory_contents(BRONZE_CONTAINER, DATASET_NAME)
+    input_paths = get_directory_contents(BRONZE_CONTAINER, dataset_name)
     csv_files = filter_csv_files(input_paths)
     save_files_as_delta_tables(spark, csv_files)
 
