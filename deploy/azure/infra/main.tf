fd78db8e2f6935327843f961a2a82c82fdc08d7c
“Rishikesh <rishikesh_singh@next.co.uk>
2023-06-08 15:28:36 +0100

    fixed: fmt

--- deploy/azure/infra/main.tf
+++ deploy/azure/infra/main.tf
@@ -256,7 +256,7 @@ resource "azurerm_role_assignment" "adb_role" {
 }
 
 resource "databricks_token" "pat" {
-  comment  = var.databricks_pat_comment
+  comment = var.databricks_pat_comment
   // 120 day token
   lifetime_seconds = 120 * 24 * 60 * 60
 }
