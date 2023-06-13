7ef73189a80329a0648e6aae68eb5fb2a0487472
Rhys Bushnell <rhys.bushnell@ensono.com>
2023-06-13 09:48:34 +0100

    switch to nonprod vnet

--- deploy/azure/infra/main.tf
+++ deploy/azure/infra/main.tf
@@ -259,22 +259,18 @@ module "adb1" {
   databricks_group_display_name            = var.databricks_group_display_name
   enable_private_network                   = true
   create_subnets                           = true
-  vnet_name                                = "testdatabricksvnet"
+  vnet_name                                = "amido-stacks-euw-de-nonprod"
   vnet_resource_group                      = "amido-stacks-euw-de-nonprod-network"
   public_subnet_name                       = "amido-stacks-euw-de-nonprod-db-public"
   private_subnet_name                      = "amido-stacks-euw-de-nonprod-db-private"
   pe_subnet_name                           = "amido-stacks-euw-de-nonprod-pe"
-  # public_subnet_prefix                     = ["10.3.3.0/24"]
-  # private_subnet_prefix                    = ["10.3.4.0/24"]
-  public_subnet_prefix  = ["10.12.0.0/24"]
-  private_subnet_prefix = ["10.12.1.0/24"]
-  pe_subnet_prefix      = ["10.12.2.0/24"]
-  # vnet_address_prefix                      = "10.3"
-  # vnet_address_prefix                   = "10.12"
-  public_network_access_enabled = true
-  create_nat                    = false
-  create_lb                     = false
-  managed_vnet                  = false
+  public_subnet_prefix                     = ["10.3.3.0/24"]
+  private_subnet_prefix                    = ["10.3.4.0/24"]
+  pe_subnet_prefix                         = ["10.3.1.0/24"]
+  public_network_access_enabled            = true
+  create_nat                               = false
+  create_lb                                = false
+  managed_vnet                             = false
 
   depends_on = [azurerm_resource_group.secure]
 }
