687c86d46f843a14f4f78acb104cd073a06b3b10
Satenderrathee <satender.rathee07@gmail.com>
2023-06-21 18:28:58 +0100

    added terraform files for ADF pipeline

---
+++ data_processing/jobs/silver/adfpipeline/main.tf
@@ -0,0 +1,36 @@
+locals {
+  factoryName = data.azurerm_data_factory.example.name
+}
+
+resource "azurerm_resource_group_template_deployment" "example" {
+  name                = "Ingest_AzureSql_Example"
+  resource_group_name = var.data_factory_resource_group_name
+  deployment_mode     = "Incremental"
+  parameters_content = jsonencode({
+
+    "factoryName" = {
+      value = local.factoryName
+    }
+    "adlsStorageAccountName" = {
+      value = var.adlsStorageAccountName
+    }
+
+    "blobStorageAccountName" = {
+      value = var.blobStorageAccountName
+    }
+
+    "databricksHost" = {
+      value = var.databricksHost
+    }
+
+    "databricksWorkspaceResourceId" = {
+      value = var.databricksWorkspaceResourceId
+    }
+
+    "enableDataQualitySilver" = {
+      value = var.enableDataQualityIngest
+    }
+
+  })
+  template_content = file("ARMTemplateForFactory.json")
+}
---
+++ data_processing/jobs/silver/adfpipeline/vars.tf
@@ -0,0 +1,42 @@
+variable "data_factory" {
+  type        = string
+  default     = "amido-stacks-dev-euw-rs"
+  description = "The name of the Azure datafactory."
+}
+
+variable "data_factory_resource_group_name" {
+
+  type        = string
+  default     = "amido-stacks-dev-euw-de"
+  description = "The name of the resource group for the datafactory "
+}
+
+variable "adlsStorageAccountName" {
+  type        = string
+  default     = "amidostacksdeveuwdeadls"
+  description = "ADLS storage account name."
+}
+
+variable "blobStorageAccountName" {
+  type        = string
+  default     = "amidostacksdeveuwdeconfi"
+  description = "Blob Config Url for ADF."
+}
+
+variable "databricksHost" {
+  type        = string
+  default     = "https://adb-XXXXXXXXXXXX.azuredatabricks.net"
+  description = "Databricks Host url id"
+}
+
+variable "databricksWorkspaceResourceId" {
+  type        = string
+  default     = "/subscriptions/XXXXXXXXXXXXX/resourceGroups/XXXXXXXXXXX/providers/Microsoft.Databricks/workspaces/XXXXXXX"
+  description = "Azure Databricks resource id."
+}
+
+variable "enableDataQualitySilver" {
+  type        = bool
+  default     = true
+  description = "Enable Data Quality Ingest."
+}
