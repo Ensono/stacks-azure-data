e91cff3ac276b90fd8ad807c8c5692ecfa469de7
Trishisingh <rishisingh901@gmail.com>
2023-06-27 16:45:34 +0100

    updated escape

--- ingest/jobs/Ingest_AzureSql_Example/adfpipeline/main.tf
+++ ingest/jobs/Ingest_AzureSql_Example/adfpipeline/main.tf
@@ -761,9 +761,9 @@ resource "azurerm_resource_group_template_deployment" "example" {
                     "newClusterNodeType": "Standard_DS3_v2",
                     "newClusterNumOfWorker": "2",
                     "newClusterSparkEnvVars": {
-                        "AZURE_CLIENT_SECRET": "`{`{secrets/key-vault-secret/service-principal-secret}`}`",
-                        "AZURE_CLIENT_ID": "`{`{secrets/key-vault-secret/azure-client-id}`}`",
-                        "AZURE_TENANT_ID": "`{`{secrets/key-vault-secret/azure-tenant-id}`}`",
+                        "AZURE_CLIENT_SECRET": "\\{\\{secrets/key-vault-secret/service-principal-secret\\}\\}",
+                        "AZURE_CLIENT_ID": "\\{\\{secrets/key-vault-secret/azure-client-id\\}\\}",
+                        "AZURE_TENANT_ID": "\\{\\{secrets/key-vault-secret/azure-tenant-id\\}\\}",
                         "ADLS_ACCOUNT": "[parameters('adlsStorageAccountName')]",
                         "BLOB_ACCOUNT": "[parameters('blobStorageAccountName')]",
                         "PYSPARK_PYTHON": "/databricks/python3/bin/python3"
