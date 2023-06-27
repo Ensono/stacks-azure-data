d69e2853f9c5f2351be47c2a199eeaa50d36a5aa
Satenderrathee <satender.rathee07@gmail.com>
2023-06-27 14:13:55 +0100

    updated

--- data_processing/jobs/silver/adfpipeline/main.tf
+++ data_processing/jobs/silver/adfpipeline/main.tf
@@ -32,5 +32,6 @@ resource "azurerm_resource_group_template_deployment" "example" {
     }
 
   })
-  template_content = file("ARMTemplateForFactory.json")
+  template_content = "${file("./ARMTemplateForFactory.json")}"
 }
+
