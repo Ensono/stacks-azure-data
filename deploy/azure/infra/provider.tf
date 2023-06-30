b960b3c59fc10e24a47495d34825fb4669d30536
Trishisingh <rishisingh901@gmail.com>
2023-06-30 14:48:54 +0100

    updated

--- deploy/azure/infra/provider.tf
+++ deploy/azure/infra/provider.tf
@@ -8,9 +8,9 @@ provider "azurerm" {
 }
 
 provider "databricks" {
-  host                        = module.adb.databricks_hosturl
-  azure_workspace_resource_id = module.adb.adb_databricks_id
-  auth_type                   = "azure-client-secret"
+ # host                        = module.adb.databricks_hosturl
+  #azure_workspace_resource_id = module.adb.adb_databricks_id
+  #auth_type                   = "azure-client-secret"
 
 }
 
