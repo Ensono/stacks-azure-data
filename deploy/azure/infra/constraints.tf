d7aaf0532a6361fc1d1340183676e2adb54db72f
Rishi <51043972+Trishisingh@users.noreply.github.com>
2023-07-03 10:46:44 +0100

    Fix/deploydatabricks (#83)
    * updated
    
    * fix: added dependslates
    
    * fix: added dependslates added user
    
    * fix: remove adb
    
    * remove adb provider
    
    * updated
    
    * removed user

--- deploy/azure/infra/constraints.tf
+++ deploy/azure/infra/constraints.tf
@@ -6,9 +6,11 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
+
     databricks = {
       source = "databricks/databricks"
     }
+
     azapi = {
       source = "Azure/azapi"
     }
