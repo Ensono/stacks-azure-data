eae03b6a6042c3deb531b5d40f2eac1e3d9a96ba
adurkan-amido <andy.durkan@amido.com>
2023-07-10 15:01:59 +0100

    Update silver pipeline for test deployments

--- de_workloads/data_processing/silver/data_factory/adf_pipelines.tf
+++ de_workloads/data_processing/silver/data_factory/adf_pipelines.tf
@@ -1,6 +1,6 @@
 resource "azurerm_data_factory_pipeline" "pipeline_silver" {
   count           = var.include_data_quality == false ? 1 : 0
-  name            = "silver"
+  name            = "NEW_silver"
   data_factory_id = data.azurerm_data_factory.factory.id
   activities_json = file("${path.module}/pipelines/silver.json")
   description     = "Bronze to Silver data transformation."
@@ -9,7 +9,7 @@ resource "azurerm_data_factory_pipeline" "pipeline_silver" {
 
 resource "azurerm_data_factory_pipeline" "pipeline_silver_dq" {
   count           = var.include_data_quality == true ? 1 : 0
-  name            = "silver_dq"
+  name            = "NEW_silver_dq"
   data_factory_id = data.azurerm_data_factory.factory.id
   activities_json = file("${path.module}/pipelines/silver_dq.json")
   description     = "Bronze to Silver data transformation, with data quality checks."
