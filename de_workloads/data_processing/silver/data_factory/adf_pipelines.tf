b282c16623e961ff70610dd02b6540d3d77cc95a
adurkan-amido <andy.durkan@amido.com>
2023-07-10 20:13:12 +0100

    Fix DQ conditional

--- de_workloads/data_processing/silver/data_factory/adf_pipelines.tf
+++ de_workloads/data_processing/silver/data_factory/adf_pipelines.tf
@@ -12,7 +12,7 @@ resource "azurerm_resource_group_template_deployment" "pipeline_silver" {
 }
 
 resource "azurerm_resource_group_template_deployment" "pipeline_silver_dq" {
-  count               = var.include_data_quality == false ? 1 : 0
+  count               = var.include_data_quality == true ? 1 : 0
   name                = "silver_dq"
   resource_group_name = var.data_factory_resource_group_name
   deployment_mode     = "Incremental"
