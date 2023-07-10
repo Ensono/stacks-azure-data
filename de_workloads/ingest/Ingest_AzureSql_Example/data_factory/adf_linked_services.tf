66109aa9d25a09e08ac97b4b793fd0c67a355998
adurkan-amido <andy.durkan@amido.com>
2023-07-10 17:05:23 +0100

    Update sql linked service to custom type

--- de_workloads/ingest/Ingest_AzureSql_Example/data_factory/adf_linked_services.tf
+++ de_workloads/ingest/Ingest_AzureSql_Example/data_factory/adf_linked_services.tf
@@ -1,11 +1,22 @@
-resource "azurerm_data_factory_linked_service_azure_sql_database" "ls_AzureSql_ExampleSource" {
-  name                     = "NEW_ls_AzureSql_ExampleSource"
-  data_factory_id          = data.azurerm_data_factory.factory.id
-  integration_runtime_name = var.integration_runtime_name
-  description              = "Azure SQL example linked service."
-  connection_string        = var.azuresql_examplesource_connectionstring
-  key_vault_password {
-    linked_service_name = var.key_vault_linked_service_name
-    secret_name         = "sql-password"
+resource "azurerm_data_factory_linked_custom_service" "ls_AzureSql_ExampleSource" {
+  name            = "NEW_ls_AzureSql_ExampleSource"
+  data_factory_id = data.azurerm_data_factory.factory.id
+  integration_runtime {
+    name = var.integration_runtime_name
   }
+  type                 = "AzureSqlDatabase"
+  description          = "Azure SQL example linked service."
+  type_properties_json = <<JSON
+{
+  "connectionString": "${var.azuresql_examplesource_connectionstring}",
+  "password": {
+      "type": "AzureKeyVaultSecret",
+      "store": {
+          "referenceName": "${var.key_vault_linked_service_name}",
+          "type": "LinkedServiceReference"
+      },
+      "secretName": "sql-password"
+  }
+}
+JSON
 }
