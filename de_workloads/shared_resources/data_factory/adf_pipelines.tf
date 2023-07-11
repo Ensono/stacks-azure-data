019cf33560dac58f5cd1ae77be391818eeee869a
Paulina Motyl <paulina.motyl@ensono.com>
2023-07-11 09:43:08 +0200

    fix

--- de_workloads/shared_resources/data_factory/adf_pipelines.tf
+++ de_workloads/shared_resources/data_factory/adf_pipelines.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group_template_deployment" "pipeline_Get_Ingest_Confi
       value = data.azurerm_data_factory.factory.name
     }
   })
-  template_content = file("${path.module}/ARMTemplateForFactory.json")
+  template_content = file("${path.module}/pipelines/ARM_Get_Ingest_Config.json")
 }
 
 resource "azurerm_data_factory_pipeline" "pipeline_Get_Ingest_Config" {
@@ -24,6 +24,18 @@ resource "azurerm_data_factory_pipeline" "pipeline_Get_Ingest_Config" {
   depends_on = [
     azurerm_data_factory_dataset_json.ds_dp_ConfigStore_Json
   ]
+}
+
+resource "azurerm_resource_group_template_deployment" "pipeline_Generate_Ingest_Query_PM" {
+  name                = "PM_Generate_Ingest_Query"
+  resource_group_name = var.data_factory_resource_group_name
+  deployment_mode     = "Incremental"
+  parameters_content = jsonencode({
+    "factoryName" = {
+      value = data.azurerm_data_factory.factory.name
+    }
+  })
+  template_content = file("${path.module}/pipelines/ARM_Generate_Ingest_Query.json")
 }
 
 resource "azurerm_data_factory_pipeline" "pipeline_Generate_Ingest_Query" {
