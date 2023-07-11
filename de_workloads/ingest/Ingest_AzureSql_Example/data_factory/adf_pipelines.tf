1cec9ca50273840f859c37933a0df855bd881ce1
Paulina Motyl <paulina.motyl@ensono.com>
2023-07-11 09:55:34 +0200

    ingest

--- de_workloads/ingest/Ingest_AzureSql_Example/data_factory/adf_pipelines.tf
+++ de_workloads/ingest/Ingest_AzureSql_Example/data_factory/adf_pipelines.tf
@@ -16,6 +16,24 @@ resource "azurerm_data_factory_pipeline" "pipeline_Ingest_AzureSql_Example" {
   ]
 }
 
+resource "azurerm_data_factory_pipeline" "pipeline_PM_Ingest_AzureSql_Example" {
+  count           = var.include_data_quality == false ? 1 : 0
+  name            = "PM_Ingest_AzureSql_Example"
+  data_factory_id = data.azurerm_data_factory.factory.id
+  activities_json = file("${path.module}/pipelines/PM_Ingest_AzureSql_Example.json")
+  description     = "Ingest from demo Azure SQL database using ingest config file."
+  folder          = "Ingest"
+  parameters = {
+    data_source_name = "Ingest_AzureSql_Example",
+    window_start     = "2020-01-01",
+    window_start     = "2020-01-31",
+    correlation_id   = ""
+  }
+  depends_on = [
+    azurerm_data_factory_custom_dataset.ds_ex_AzureSql_ExampleSource
+  ]
+}
+
 resource "azurerm_data_factory_pipeline" "pipeline_Ingest_AzureSql_Example_DQ" {
   count           = var.include_data_quality == true ? 1 : 0
   name            = "NEW_Ingest_AzureSql_Example_DQ"
