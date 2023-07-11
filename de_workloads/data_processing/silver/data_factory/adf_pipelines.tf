d65f5c5bc4189ca8666f7bd4dd3b0bbc15739b0d
Paulina Motyl <paulina.motyl@ensono.com>
2023-07-11 16:36:30 +0200

    unique deployment names

--- de_workloads/data_processing/silver/data_factory/adf_pipelines.tf
+++ de_workloads/data_processing/silver/data_factory/adf_pipelines.tf
@@ -1,6 +1,9 @@
+resource "random_uuid" "deployment" {
+}
+
 resource "azurerm_resource_group_template_deployment" "pipeline_silver" {
   count               = var.include_data_quality == false ? 1 : 0
-  name                = "silver"
+  name                = "silver-${random_uuid.deployment.result}"
   resource_group_name = var.data_factory_resource_group_name
   deployment_mode     = "Incremental"
   parameters_content = jsonencode({
@@ -13,7 +16,7 @@ resource "azurerm_resource_group_template_deployment" "pipeline_silver" {
 
 resource "azurerm_resource_group_template_deployment" "pipeline_silver_dq" {
   count               = var.include_data_quality == true ? 1 : 0
-  name                = "silver_dq"
+  name                = "silver_dq-${random_uuid.deployment.result}"
   resource_group_name = var.data_factory_resource_group_name
   deployment_mode     = "Incremental"
   parameters_content = jsonencode({
