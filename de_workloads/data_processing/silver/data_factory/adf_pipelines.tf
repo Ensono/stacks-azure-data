80d7d27f2ac622ec01e6148af114db26b2e2f9b3
Paulina Motyl <paulina.motyl@ensono.com>
2023-07-11 16:37:37 +0200

    deploy DQ and non-DQ

--- de_workloads/data_processing/silver/data_factory/adf_pipelines.tf
+++ de_workloads/data_processing/silver/data_factory/adf_pipelines.tf
@@ -2,7 +2,7 @@ resource "random_uuid" "deployment" {
 }
 
 resource "azurerm_resource_group_template_deployment" "pipeline_silver" {
-  count               = var.include_data_quality == false ? 1 : 0
+  count               = var.include_data_quality == false ? 1 : 1
   name                = "silver-${random_uuid.deployment.result}"
   resource_group_name = var.data_factory_resource_group_name
   deployment_mode     = "Incremental"
@@ -15,7 +15,7 @@ resource "azurerm_resource_group_template_deployment" "pipeline_silver" {
 }
 
 resource "azurerm_resource_group_template_deployment" "pipeline_silver_dq" {
-  count               = var.include_data_quality == true ? 1 : 0
+  count               = var.include_data_quality == true ? 1 : 1
   name                = "silver_dq-${random_uuid.deployment.result}"
   resource_group_name = var.data_factory_resource_group_name
   deployment_mode     = "Incremental"
