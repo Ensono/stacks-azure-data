bf9ba804d7ab9ad7045e193193520c03a1171077
Paulina Motyl <paulina.motyl@ensono.com>
2023-07-11 10:44:18 +0200

    cleanup

--- de_workloads/shared_resources/data_factory/adf_pipelines.tf
+++ de_workloads/shared_resources/data_factory/adf_pipelines.tf
@@ -1,5 +1,5 @@
-resource "azurerm_resource_group_template_deployment" "pipeline_Get_Ingest_Config_PM" {
-  name                = "PM_Get_Ingest_Config"
+resource "azurerm_resource_group_template_deployment" "pipeline_Get_Ingest_Config" {
+  name                = "Get_Ingest_Config"
   resource_group_name = var.data_factory_resource_group_name
   deployment_mode     = "Incremental"
   parameters_content = jsonencode({
@@ -10,24 +10,8 @@ resource "azurerm_resource_group_template_deployment" "pipeline_Get_Ingest_Confi
   template_content = file("${path.module}/pipelines/ARM_Get_Ingest_Config.json")
 }
 
-resource "azurerm_data_factory_pipeline" "pipeline_Get_Ingest_Config" {
-  name            = "NEW_Get_Ingest_Config"
-  data_factory_id = data.azurerm_data_factory.factory.id
-  activities_json = file("${path.module}/pipelines/Get_Ingest_Config.json")
-  description     = "Retrieve ingest config from the config store for the specified data source."
-  folder          = "Utilities"
-  parameters = {
-    config_container = "config",
-    config_path      = "",
-    config_file      = ""
-  }
-  depends_on = [
-    azurerm_data_factory_dataset_json.ds_dp_ConfigStore_Json
-  ]
-}
-
-resource "azurerm_resource_group_template_deployment" "pipeline_Generate_Ingest_Query_PM" {
-  name                = "PM_Generate_Ingest_Query"
+resource "azurerm_resource_group_template_deployment" "pipeline_Generate_Ingest_Query" {
+  name                = "Generate_Ingest_Query"
   resource_group_name = var.data_factory_resource_group_name
   deployment_mode     = "Incremental"
   parameters_content = jsonencode({
@@ -36,20 +20,4 @@ resource "azurerm_resource_group_template_deployment" "pipeline_Generate_Ingest_
     }
   })
   template_content = file("${path.module}/pipelines/ARM_Generate_Ingest_Query.json")
-}
-
-resource "azurerm_data_factory_pipeline" "pipeline_Generate_Ingest_Query" {
-  name            = "NEW_Generate_Ingest_Query"
-  data_factory_id = data.azurerm_data_factory.factory.id
-  activities_json = file("${path.module}/pipelines/Generate_Ingest_Query.json")
-  description     = "Generate an ingest query from the provided ingest entity configuration."
-  folder          = "Utilities"
-  parameters = {
-    ingest_entity_config = "", # this param was previously Object type, may need converting in ADF
-    window_start         = "",
-    window_end           = ""
-  }
-  variables = {
-    query = ""
-  }
 }
