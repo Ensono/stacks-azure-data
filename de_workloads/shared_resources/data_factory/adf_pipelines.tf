d3249cf4132dc3dc8fba70758d317bae4af2d782
adurkan-amido <andy.durkan@amido.com>
2023-07-13 17:30:18 +0100

    Fix pipeline name

--- de_workloads/shared_resources/data_factory/adf_pipelines.tf
+++ de_workloads/shared_resources/data_factory/adf_pipelines.tf
@@ -1,5 +1,5 @@
-resource "azurerm_resource_group_template_deployment" "pipeline_Generate_Ingest_Query" {
-  name                = "pipeline_Generate_Ingest_Query"
+resource "azurerm_resource_group_template_deployment" "pipeline_Get_Ingest_Config" {
+  name                = "pipeline_Get_Ingest_Config"
   resource_group_name = var.data_factory_resource_group_name
   deployment_mode     = var.arm_deployment_mode
   parameters_content = jsonencode({
