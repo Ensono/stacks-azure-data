753707d4123b01073ca5ade1ec09dff5a39db5e8
Paulina Motyl <paulina.motyl@ensono.com>
2023-07-13 19:19:07 +0200

    tests

--- de_workloads/ingest_template/Ingest_AzureSql_SourceName/data_factory/test_jinja.py
+++ de_workloads/ingest_template/Ingest_AzureSql_SourceName/data_factory/test_jinja.py
@@ -1,12 +1,28 @@
-import jinja2
+from jinja2 import Environment, Undefined
 import yaml
+
+
+class SilentUndefined(Undefined):
+    def __str__(self):
+        return (
+            self._undefined_name
+            if self._undefined_name is not None
+            else super().__str__()
+        )
+
+    def __getattr__(self, name):
+        return self.__class__(name=f"{self}.{name}")
+
+    def __getitem__(self, name):
+        return self.__class__(name=f"{self}[{name}]")
+
 
 with open("../config.yaml", "r") as file:
     config = yaml.safe_load(file)
 
-environment = jinja2.Environment()
+environment = Environment(undefined=SilentUndefined)
 with open("adf_pipelines.tf.template", "r") as file:
     content = file.read()
 
 template = environment.from_string(content)
-print(template.render(config))
+print(template.render())
