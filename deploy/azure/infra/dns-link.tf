99ed6fb4a70762af097c8ff47a2d63988304d52f
Trishisingh <rishisingh901@gmail.com>
2023-07-13 10:21:22 +0100

    updated name

--- deploy/azure/infra/dns-link.tf
+++ deploy/azure/infra/dns-link.tf
@@ -1,6 +1,6 @@
 resource "azurerm_private_dns_zone_virtual_network_link" "dev-privatelink-dns" {
   for_each              = var.link_dns_dev_spoke_network == true ? toset(var.dns_zone_name) : toset([])
-  name                  = each.key
+  name                  = data.azurerm_virtual_network.dev.name
   resource_group_name   = var.dns_zone_resource_group
   registration_enabled  = false # Auto registration_enabled set to false as we cannot add multiple Private DNS to 1 Vnet
   private_dns_zone_name = each.key
@@ -10,7 +10,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "dev-privatelink-dns" {
 
 resource "azurerm_private_dns_zone_virtual_network_link" "prod-privatelink-dns" {
   for_each              = var.link_dns_prod_spoke_network == true ? toset(var.dns_zone_name) : toset([])
-  name                  = each.key
+  name                  = data.azurerm_virtual_network.prod.name
   resource_group_name   = var.dns_zone_resource_group
   registration_enabled  = false # Auto registration_enabled set to false as we cannot add multiple Private DNS to 1 Vnet
   private_dns_zone_name = each.key
