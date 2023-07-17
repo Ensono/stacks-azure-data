53bd513767ea9358d786cdac0bf43b74e4625fc1
Paulina Motyl <paulina.motyl@ensono.com>
2023-07-17 13:22:02 +0200

    templates

---
+++ de_templates/ingest/Ingest_AzureSql_SourceName_DQ/spark_jobs/ingest_dq.py
@@ -0,0 +1,16 @@
+from pysparkle.pysparkle_cli import cli
+
+
+def call_pysparkle_entrypoint():
+    cli(
+        [
+            "data-quality",
+            "--config-path",
+            "ingest/Ingest_AzureSql_Example/data_quality/ingest_dq.json",
+        ],
+        standalone_mode=False,
+    )
+
+
+if __name__ == "__main__":
+    call_pysparkle_entrypoint()
