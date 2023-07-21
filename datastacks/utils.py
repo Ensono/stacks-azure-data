2e9c2722178558ade8ddf0b148b0bd6d65aaf7f6
Lorraine Snaddon <lorraine.snaddon@ensono.com>
2023-07-21 12:17:26 +0100

    Add docstring and typehints

--- datastacks/utils.py
+++ datastacks/utils.py
@@ -4,7 +4,15 @@ import yaml
 from pathlib import Path
 
 
-def render_template_components(config, template_source_path, target_dir):
+def render_template_components(config: dict, template_source_path: str, target_dir: str) -> None:
+    """Renders all templates within a given path with provided config, and saves results into a new target path,
+    while maintaining folder structure and removing jinja file extensions.
+
+    Args:
+        config: Dict of config containing required templating params
+        template_source_path: path containing templates to be rendered
+        target_dir: Directory to render templates into
+    """
     Path(target_dir).mkdir(parents=True, exist_ok=True)
     templateLoader = FileSystemLoader(searchpath=str(Path(template_source_path).absolute()))
     templateEnv = Environment(loader=templateLoader, autoescape=True)
