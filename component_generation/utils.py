836f43f823f90f0a982062d4e4a1eeb8345c17d0
Lorraine Snaddon <lorraine.snaddon@ensono.com>
2023-07-21 09:16:40 +0100

    Stop using SilentUndefined

--- component_generation/utils.py
+++ component_generation/utils.py
@@ -1,32 +1,13 @@
 from enum import Enum
 from jinja2 import Environment, FileSystemLoader, Undefined
 import yaml
-import os
 from pathlib import Path
 
-
-class SilentUndefined(Undefined):
-    def __str__(self):
-        return (
-            self._undefined_name
-            if self._undefined_name is not None
-            else super().__str__()
-        )
-
-    def __getattr__(self, name):
-        return self.__class__(name=f"{self}.{name}")
-
-    def __getitem__(self, name):
-        return self.__class__(name=f"{self}[{name}]")
-
-
-with open("component_generation/test_config_ingest.yaml", "r") as file:
-    config = yaml.safe_load(file)
 
 def render_template_component(config, template_source_path, target_dir):
     Path(target_dir).mkdir(parents=True, exist_ok=True)
     templateLoader = FileSystemLoader(searchpath=str(Path(template_source_path).absolute()))
-    templateEnv = Environment(loader=templateLoader, undefined=SilentUndefined)
+    templateEnv = Environment(loader=templateLoader, autoescape=True)
 
     template_list = templateEnv.list_templates(extensions='.jinja')
     for temp in template_list:
