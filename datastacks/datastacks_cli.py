38dda4126eb3e0258b6c7c1d2429673caf12ad02
Lorraine Snaddon <lorraine.snaddon@ensono.com>
2023-07-25 15:11:17 +0100

    PR updates

--- datastacks/datastacks_cli.py
+++ datastacks/datastacks_cli.py
@@ -7,7 +7,7 @@ from click_loglevel import LogLevel
 
 from datastacks.logger import setup_logger
 from datastacks.utils import render_template_components
-
+from datastacks.config import IngestConfig
 
 @click.group()
 @click.option("--log-level", "-l", type=LogLevel(), default=logging.INFO)
@@ -19,9 +19,9 @@ def generate():
     pass
 
 @generate.command()
-@click.option("--config-path", "-c", type=str, help="Absolute path to config file on local machine")
+@click.option("--config", "-c", type=str, help="Absolute path to config file on local machine")
 @click.option("--data-quality/--no-data-quality", "-dq/-ndq", default=False, help="Flag to determine whether to include data quality in template")
-def ingest(config_path, data_quality):
+def ingest(config, data_quality):
     """Generate new ingest pipeline"""
     if data_quality:
         template_source_folder = "Ingest_SourceType_SourceName_DQ"
@@ -29,16 +29,18 @@ def ingest(config_path, data_quality):
         template_source_folder = "Ingest_SourceType_SourceName"
     
     click.echo("Reading config from provided path...")
-    with open(config_path, "r") as file:
-        config = yaml.safe_load(file)
+    with open(config, "r") as file:
+        config_dict = yaml.safe_load(file)
+    config = IngestConfig.parse_obj(config_dict)
+
     click.echo("Successfully read config file.\n")
 
     template_source_path = f"de_templates/ingest/{template_source_folder}/"
-    target_dir = f"de_workloads/ingest/{config['pipeline']}"
+    target_dir = f"de_workloads/ingest/ingest_{config.common.dataset_name}"
 
-    click.echo("Rendering component templates with provided config...")
+    click.echo(f"Generating workload components for pipeline ingest_{config.common.dataset_name}...")
     render_template_components(config, template_source_path, target_dir)
-    click.echo("Successfully rendered new component!")
+    click.echo(f"Successfully generated workload components: {target_dir}")
 
 
 if __name__ == "__main__":
