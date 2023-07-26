0437e55812197bcf23dfd88a5f9404493266b5d9
Lorraine Snaddon <lorraine.snaddon@ensono.com>
2023-07-26 09:58:19 +0100

    Make black

--- datastacks/utils.py
+++ datastacks/utils.py
@@ -6,7 +6,9 @@ from jinja2 import Environment, FileSystemLoader
 from pathlib import Path
 
 
-def render_template_components(config: dict, template_source_path: str, target_dir: str) -> None:
+def render_template_components(
+    config: dict, template_source_path: str, target_dir: str
+) -> None:
     """Renders all templates within a given path with provided config, and saves results into a new target path,
     while maintaining folder structure and removing jinja file extensions.
 
@@ -16,29 +18,35 @@ def render_template_components(config: dict, template_source_path: str, target_d
         target_dir: Directory to render templates into
     """
     Path(target_dir).mkdir(parents=True, exist_ok=True)
-    template_loader = FileSystemLoader(searchpath=str(Path(template_source_path).absolute()))
+    template_loader = FileSystemLoader(
+        searchpath=str(Path(template_source_path).absolute())
+    )
     template_env = Environment(loader=template_loader, autoescape=True)
 
-    template_list = template_env.list_templates(extensions='.jinja')
+    template_list = template_env.list_templates(extensions=".jinja")
     for temp in template_list:
         template = template_env.get_template(temp)
-        template_filepath = Path(template.filename.split(template_source_path,1)[1])
+        template_filepath = Path(template.filename.split(template_source_path, 1)[1])
         template_path = template_filepath.parent
         template_filename = template_filepath.stem
-        Path(target_dir/template_path).mkdir(parents=True, exist_ok=True)
-        template.stream(config).dump(f'{target_dir}/{template_path}/{template_filename}')
+        Path(target_dir / template_path).mkdir(parents=True, exist_ok=True)
+        template.stream(config).dump(
+            f"{target_dir}/{template_path}/{template_filename}"
+        )
 
 
-def generate_pipeline(config_path: str, dq_flag: bool, template_source_folder: str, stage_name: str) -> None:
+def generate_pipeline(
+    config_path: str, dq_flag: bool, template_source_folder: str, stage_name: str
+) -> None:
     """Reads in config from given file, renders templates for new pipeline,
        and writes out to new path
     Args:
         config_path: Path to config file containing templating params
         dq_flag: Flag indicating whether to include data quality components or not
-        template_source_folder: Name of the folder within the template directory which 
+        template_source_folder: Name of the folder within the template directory which
             contains the templates to be rendered
         stage_name: Name of the pipeline stage eg. Ingest
-    """    
+    """
     click.echo("Reading config from provided path...")
     with open(config_path, "r") as file:
         config_dict = yaml.safe_load(file)
@@ -49,7 +57,9 @@ def generate_pipeline(config_path: str, dq_flag: bool, template_source_folder: s
     template_source_path = f"de_templates/{stage_name}/{template_source_folder}/"
     target_dir = f"de_workloads/{stage_name}/{stage_name}_{config.dataset_name}"
 
-    click.echo(f"Generating workload components for pipeline {stage_name}_{config.dataset_name}...")
+    click.echo(
+        f"Generating workload components for pipeline {stage_name}_{config.dataset_name}..."
+    )
     render_template_components(config, template_source_path, target_dir)
     if dq_flag:
         template_source_folder = f"{template_source_folder}_DQ"
