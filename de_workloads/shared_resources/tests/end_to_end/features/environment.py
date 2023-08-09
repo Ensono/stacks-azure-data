f2885730c7e93de4a1ddab8a1c5d8d5d719aedfb
adurkan-amido <andy.durkan@amido.com>
2023-08-09 20:48:14 +0100

    shared_steps tests tidy up

--- de_workloads/shared_resources/tests/end_to_end/features/environment.py
+++ de_workloads/shared_resources/tests/end_to_end/features/environment.py
@@ -2,8 +2,8 @@ from behave import use_fixture
 
 from fixtures import azure_adls_clean_up
 
-SQL_DB_INGEST_DIRECTORY_NAME = "example_azuresql_1"
+TEST_DIRECTORY_NAME = "shared_steps_test"
 
 
 def before_scenario(context, scenario):
-    use_fixture(azure_adls_clean_up, context, SQL_DB_INGEST_DIRECTORY_NAME)
+    use_fixture(azure_adls_clean_up, context, TEST_DIRECTORY_NAME)
