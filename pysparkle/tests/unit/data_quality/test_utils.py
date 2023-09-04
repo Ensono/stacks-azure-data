c1bbb3ba730ed3ad40ca85383ff08bdf3c1639fd
Lorraine Snaddon <lorraine.snaddon@ensono.com>
2023-09-04 16:29:57 +0100

    Update E2E tests

--- pysparkle/tests/unit/data_quality/test_utils.py
+++ pysparkle/tests/unit/data_quality/test_utils.py
@@ -1,7 +1,16 @@
 import pytest
 
+from datetime import date
 from great_expectations.core.expectation_suite import ExpectationSuite
-from pysparkle.data_quality.utils import add_expectations_for_columns, add_expectation_suite, execute_validations
+from great_expectations.core.expectation_validation_result import ExpectationValidationResult
+from great_expectations.core.expectation_configuration import ExpectationConfiguration
+
+from pysparkle.data_quality.utils import (
+    add_expectations_for_columns,
+    add_expectation_suite,
+    execute_validations,
+    publish_quality_results_table,
+)
 
 
 def test_create_datasource_context(dq_config, datasource_context):
@@ -64,3 +73,95 @@ def test_execute_validations(spark, dq_config, datasource_context, data, expecte
     context = add_expectation_suite(datasource_context, dq_config.datasource_config[0])
     result = execute_validations(context, dq_config.datasource_config[0], df)
     assert result["success"] is expected
+
+
+@pytest.fixture(scope="session")
+def expectation_results():
+    result1 = {
+        "element_count": 5,
+        "unexpected_count": 0,
+        "unexpected_percent": 0,
+        "partial_unexpected_list": [],
+        "missing_count": 0,
+        "missing_percent": 0.0,
+        "unexpected_percent_total": 0,
+        "unexpected_percent_nonmissing": 0,
+        "partial_unexpected_index_list": None,
+        "partial_unexpected_counts": [],
+        "unexpected_list": [],
+        "unexpected_index_list": None,
+    }
+    kwargs1 = {"column": "col2", "result_format": "COMPLETE", "batch_id": "batch"}
+    expectation_type = "expect_column_values_to_be_in_set"
+    expectation_config1 = ExpectationConfiguration(kwargs=kwargs1, expectation_type=expectation_type)
+    validator_result1 = ExpectationValidationResult(result=result1, expectation_config=expectation_config1)
+    result2 = {
+        "element_count": 5,
+        "unexpected_count": 1,
+        "unexpected_percent": 20,
+        "partial_unexpected_list": ["wrong"],
+        "missing_count": 0,
+        "missing_percent": 0.0,
+        "unexpected_percent_total": 20,
+        "unexpected_percent_nonmissing": 20,
+        "partial_unexpected_index_list": None,
+        "partial_unexpected_counts": [
+            {"value": "wrong", "count": 1},
+        ],
+        "unexpected_list": [
+            "wrong",
+        ],
+        "unexpected_index_list": None,
+    }
+    kwargs2 = {
+        "column": "col1",
+        "result_format": "COMPLETE",
+        "batch_id": "batch",
+        "mostly": 0.99,
+        "value_set": ["right", "blah", "A", "C", "E"],
+    }
+    expectation_config2 = ExpectationConfiguration(kwargs=kwargs2, expectation_type=expectation_type)
+    validator_result2 = ExpectationValidationResult(
+        success=False, result=result2, expectation_config=expectation_config2
+    )
+    expectation_results = [validator_result1, validator_result2]
+    return expectation_results
+
+
+def test_publish_quality_results_table(mocker, spark, expectation_results):
+    base_path = "a/fake/path/"
+    datasource_name = "fake_database"
+    data_quality_run_date = date(year=2000, month=1, day=1)
+
+    expected_cols = [
+        "DataQualityRunDate",
+        "Datamart",
+        "ColumnName",
+        "Validator",
+        "ValueSet",
+        "Threshold",
+        "FailureCount",
+        "FailurePercent",
+        "Success",
+    ]
+    expected_data = [
+        (
+            data_quality_run_date,
+            datasource_name,
+            "col1",
+            "expect_column_values_to_be_in_set",
+            "[right, blah, A, C, E]",
+            "0.99",
+            "1",
+            "20",
+            False,
+        )
+    ]
+
+    expected_failure = spark.createDataFrame(data=expected_data, schema=expected_cols)
+
+    mocker.patch("pysparkle.data_quality.utils.save_dataframe_as_delta")
+    failed_validations = publish_quality_results_table(
+        spark, base_path, datasource_name, expectation_results, data_quality_run_date
+    )
+    assert sorted(failed_validations.collect()) == sorted(expected_failure.collect())
