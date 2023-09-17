f38822ce19e06e9ca2e3c15cf9e9d6ace1e2bd71
Satenderrathee <satender.rathee07@gmail.com>
2023-09-17 19:58:07 +0100

    updated

--- deploy/azure/infra/vars.tf
+++ deploy/azure/infra/vars.tf
@@ -525,7 +525,7 @@ variable "dfs_dns_resource_group_name" {
 
 variable "kv_public_network_access_enabled" {
   type        = bool
-  default     = false
+  default     = true
   description = "Allow public network access to Key Vault. Set as true or false."
 }
 
