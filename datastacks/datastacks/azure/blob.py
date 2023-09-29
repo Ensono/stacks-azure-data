3d3c4ec2976434383b7c4decfe52ad5f2cf13975
adurkan-amido <andy.durkan@amido.com>
2023-09-29 14:21:09 +0100

    add delete blob

--- datastacks/datastacks/azure/blob.py
+++ datastacks/datastacks/azure/blob.py
@@ -8,27 +8,47 @@ def upload_file_to_blob(
     blob_service_client: BlobServiceClient,
     container_name: str,
     target_dir: str,
-    config_file_path: str,
+    local_file_path: str,
     overwrite: bool = True,
-) -> bool:
+):
     """Upload a file to blob storage.
 
     Args:
         blob_service_client: BlobServiceClient
         container_name: Container name
         target_dir: Directory to load into
-        config_file_path: Filepath of the file to upload
+        local_file_path: Path to the file to upload
         overwrite: Overwrite the file if it already exists
-
-    Returns:
-        Boolean reflecting whether upload was successful
     """
-    target_blob_path = f"{target_dir}/{config_file_path}"
-
+    file_name = local_file_path.rsplit("/", 1)[-1]
+    target_blob_path = f"{target_dir}/{file_name}"
     container_client = blob_service_client.get_container_client(container_name)
     blob_client = container_client.get_blob_client(target_blob_path)
 
-    with open(config_file_path, "rb") as file:
+    with open(local_file_path, "rb") as file:
         blob_client.upload_blob(file, overwrite=overwrite)
-    logger.info(f"Uploaded {config_file_path} to {container_name}/{target_blob_path}.")
+    logger.info(f"Uploaded {local_file_path} to {container_name}/{target_blob_path}.")
+
+
+def delete_blob_prefix(blob_service_client: BlobServiceClient, container_name: str, blob_prefix: str) -> bool:
+    """Delete files with a given prefix from blob storage.
+
+    Args:
+        blob_service_client: BlobServiceClient
+        container_name: Container name
+        blob_prefix: Blob prefix in the container to delete
+    """
+    container_client = blob_service_client.get_container_client(container_name)
+    blob_list = container_client.list_blobs(name_starts_with=blob_prefix)
+    if not blob_list:
+        logger.info(f"No blobs exist with prefix {blob_prefix} in container {container_name}")
+    else:
+        try:
+            for blob in blob_list:
+                logger.info(f"deleting {blob}")
+                container_client.delete_blob(blob.name)
+            logger.info(f"All blobs with prefix {blob_prefix} deleted successfully from container {container_name}")
+        except Exception as e:
+            logger.warning(f"Error deleting directory '{blob_prefix}': {str(e)}")
+            return False
     return True
