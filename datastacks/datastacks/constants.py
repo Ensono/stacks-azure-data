18dd2c1072b597054c1e3be44c5ffe390c996ed4
adurkan-amido <andy.durkan@amido.com>
2023-10-09 15:31:06 +0100

    add output path to ingest

--- datastacks/datastacks/constants.py
+++ datastacks/datastacks/constants.py
@@ -1,5 +1,6 @@
 import os
 
+# Azure environment variables
 AZURE_SUBSCRIPTION_ID = os.environ.get("AZURE_SUBSCRIPTION_ID")
 AZURE_RESOURCE_GROUP_NAME = os.environ.get("AZURE_RESOURCE_GROUP_NAME")
 AZURE_DATA_FACTORY_NAME = os.environ.get("AZURE_DATA_FACTORY_NAME")
@@ -7,11 +8,15 @@ AZURE_REGION_NAME = os.environ.get("AZURE_REGION_NAME")
 AZURE_STORAGE_ACCOUNT_NAME = os.environ.get("AZURE_STORAGE_ACCOUNT_NAME")
 AZURE_CONFIG_ACCOUNT_NAME = os.environ.get("AZURE_CONFIG_ACCOUNT_NAME")
 
-RAW_CONTAINER_NAME = "raw"
+# ADLS constants
+BRONZE_CONTAINER_NAME = "raw"
+SILVER_CONTAINER_NAME = "staging"
+GOLD_CONTAINER_NAME = "curated"
 ADLS_URL = f"https://{AZURE_STORAGE_ACCOUNT_NAME}.dfs.core.windows.net"
+
+# Config storage constants
 CONFIG_CONTAINER_NAME = "config"
 CONFIG_BLOB_URL = f"https://{AZURE_CONFIG_ACCOUNT_NAME}.blob.core.windows.net"
 
+# Automated test config
 AUTOMATED_TEST_OUTPUT_DIRECTORY_PREFIX = "automated_test"
-
-CONFIG_CONTAINER_NAME = "config"
