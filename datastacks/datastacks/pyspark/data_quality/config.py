1cfd2a6691527a2b2a861e1226e0625c052af67b
Andy Durkan <108685738+adurkan-amido@users.noreply.github.com>
2023-10-11 09:10:30 +0100

    Fix/7095 dq e2e test data pt2 (#220)
    * add output path to ingest
    
    * add new tasks to ingest adf
    
    * add output path to databricks vars
    
    * add output path to processing examples
    
    * add output path to processing templates
    
    * use container name constants
    
    * update default output path
    
    * remove output path from processing pipelines
    
    * add get_data_factory_param
    
    * dq input path and adls path replacement
    
    * update unit test conf
    
    * add replace_adls_data_location
    
    * add container arg to adls clean up
    
    * fix data_quality_main args
    
    * add test flag condition to replace adls location
    
    * docs updates

--- datastacks/datastacks/pyspark/data_quality/config.py
+++ datastacks/datastacks/pyspark/data_quality/config.py
@@ -14,7 +14,9 @@ class ValidationConfig(BaseModel):
 class DatasourceConfig(BaseModel):
     datasource_name: str = Field(description="Data asset name (e.g. table or file name).")
     datasource_type: str = Field(description="Source system type (e.g. table, parquet, csv).")
-    data_location: str = Field(description="Path to the given data asset or a fully qualified table name.")
+    data_location: str = Field(
+        description="Path to the given data asset within the dq_input_path, or a fully qualified table name."
+    )
     expectation_suite_name: str
     validation_config: list[ValidationConfig]
 
@@ -22,5 +24,6 @@ class DatasourceConfig(BaseModel):
 class Config(BaseModel):
     gx_directory_path: str = Field(description="Path to GX Metadata Store.")
     dataset_name: str
-    dq_output_path: str = Field(description="Path to where data quality results should be written")
+    dq_input_path: str = Field(description="Path to where the input data is stored.")
+    dq_output_path: str = Field(description="Path to where data quality results will be written.")
     datasource_config: list[DatasourceConfig]
