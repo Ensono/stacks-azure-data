2a4b8b2303c58a5f5494c15cd29ff0803a299639
adurkan-amido <andy.durkan@amido.com>
2023-10-12 21:40:05 +0100

    add e2e test data

--- de_workloads/shared_resources/tests/end_to_end/features/environment.py
+++ de_workloads/shared_resources/tests/end_to_end/features/environment.py
@@ -5,7 +5,7 @@ from behave.runner import Context
 from stacks.data.behave.fixtures import azure_blob_config_prepare
 
 DATA_TARGET_DIRECTORY = "shared_steps_test"
-DATA_LOCAL_DIRECTORY = "datastacks/tests/data/ingest_sources"
+DATA_LOCAL_DIRECTORY = "de_workloads/shared_resources/tests/data/ingest_sources"
 
 
 def before_feature(context: Context, scenario: Scenario):
