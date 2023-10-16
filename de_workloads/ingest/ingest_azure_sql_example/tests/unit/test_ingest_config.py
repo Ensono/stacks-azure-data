26ef8fb012cad9fd70dfb103e473a069b2002d6d
adurkan-amido <andy.durkan@amido.com>
2023-10-16 10:39:08 +0100

    rename dq config schema

--- de_workloads/ingest/ingest_azure_sql_example/tests/unit/test_ingest_config.py
+++ de_workloads/ingest/ingest_azure_sql_example/tests/unit/test_ingest_config.py
@@ -11,7 +11,7 @@ CONFIG_PATH = "de_workloads/ingest/ingest_azure_sql_example/config"
 INGEST_CONFIG_PATH = f"{CONFIG_PATH}/ingest_sources"
 INGEST_DQ_CONFIG_PATH = f"{CONFIG_PATH}/data_quality"
 INGEST_CONFIG_SCHEMA = f"{CONFIG_PATH}/schema/ingest_config_schema.json"
-INGEST_DQ_CONFIG_SCHEMA = f"{CONFIG_PATH}/schema/ingest_dq_schema.json"
+INGEST_DQ_CONFIG_SCHEMA = f"{CONFIG_PATH}/schema/data_quality_config_schema.json"
 
 
 @pytest.mark.parametrize(
