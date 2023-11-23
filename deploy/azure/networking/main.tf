7f05b7f9782a47ac4b6c3d24daff99015227cb25
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2023-11-23 15:09:29 +0000

    Moved the location shortnames into locals as these do not need to be overridden as a variable
    Signed-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- deploy/azure/networking/main.tf
+++ deploy/azure/networking/main.tf
@@ -4,7 +4,7 @@ module "default_label" {
   source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=0.24.1"
   namespace  = format("%s-%s", var.name_company, var.name_project)
   stage      = var.stage
-  name       = "${lookup(var.location_name_map, var.resource_group_location)}-${var.name_component}"
+  name       = "${lookup(local.location_name_map, var.resource_group_location)}-${var.name_component}"
   attributes = var.attributes
   delimiter  = "-"
   tags       = var.tags
