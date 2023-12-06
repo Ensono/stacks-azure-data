62f7a6db7382e5ec8fafed65ab808fe62aeb5ea3
Zaid Khider <zaid.khider@ensono.com>
2023-12-06 14:01:56 +0000

    update automatic variable

--- deploy/azure/networking/main.tf
+++ deploy/azure/networking/main.tf
@@ -1,6 +1,6 @@
 locals {
   # Get the hub network name which will be used later to deploy the vmss into it
-  hub_network_name = [for network in var.network_details : network.name if network.is_hub == true][0]
+  hub_network_name = [for network in local.network_details : network.name if network.is_hub == true][0]
 }
 
 # Naming convention
@@ -16,19 +16,20 @@ module "default_label" {
 }
 
 module "networking" {
-  source                  = "git::https://github.com/amido/stacks-terraform//azurerm/modules/azurerm-hub-spoke?ref=fix/deployment"
+  source                  = "git::https://github.com/ensono/stacks-terraform//azurerm/modules/azurerm-hub-spoke?ref=fix/deployment"
   enable_private_networks = var.enable_private_networks ## NOTE setting this value to false will cause no resources to be created !!
   network_details         = local.network_details
   resource_group_location = var.resource_group_location
   create_hub_fw           = var.create_hub_fw
   create_fw_public_ip     = var.create_fw_public_ip
-  dns_zone_name           = [module.default_label.id]
+  dns_zone_name           = ["${module.default_label.id}.com"]
+  merge_dns_zones = true
   link_dns_network        = var.link_dns_network
 }
 
 module "vmss" {
   count                        = var.enable_private_networks ? 1 : 0
-  source                       = "git::https://github.com/amido/stacks-terraform//azurerm/modules/azurerm-vmss?ref=fix/deployment"
+  source                       = "git::https://github.com/ensono/stacks-terraform//azurerm/modules/azurerm-vmss?ref=fix/deployment"
   vmss_name                    = module.default_label.id
   vmss_resource_group_name     = length(module.networking) > 0 ? module.networking.vnets[module.networking.hub_net_name].vnet_resource_group_name : ""
   vmss_resource_group_location = var.resource_group_location
