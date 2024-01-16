c028fbc1bfe43fd759996f2b778897531a277706
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2024-01-16 17:10:38 +0000

    Updates to allow deployment of the resources using a public network ***NO_CI***

--- deploy/azure/infra/provider.tf
+++ deploy/azure/infra/provider.tf
@@ -4,7 +4,16 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+
+    # Enable the purging of secrets on delete
+    # This is so that when a Key Vault is destroyed it can be recreated
+    # without the keys and secrets coming back from a soft delete
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = false
+    }
+  }
 }
 
 provider "databricks" {
