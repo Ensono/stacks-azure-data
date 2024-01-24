768a4032d300b121fc532ea61dcbba82845b9b82
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2024-01-24 09:53:12 +0000

    Corrected linting issues
    Signed-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- deploy/azure/infra/data.tf
+++ deploy/azure/infra/data.tf
@@ -1,53 +1,42 @@
 data "azurerm_client_config" "current" {}
 
 data "azurerm_subnet" "pe_subnet" {
-  count = var.enable_private_networks ? 1 : 0
   name                 = var.subnet_name
   virtual_network_name = var.vnet_name
   resource_group_name  = var.vnet_resource_group_name
 }
 
-/*
-data "azurerm_private_dns_zone" "private_dns" {
-  name                = var.dns_zone_namede
-  resource_group_name = var.dns_zone_resource_group
-
-}
-
-*/
-
 data "azurerm_virtual_network" "vnet" {
-  count = var.enable_private_networks ? 1 : 0
   name                = var.vnet_name
   resource_group_name = var.vnet_resource_group_name
 }
 
 data "azurerm_private_dns_zone" "dfs_private_zone" {
-  count = var.enable_private_networks ? 1 : 0
   name                = var.dfs_private_zone
   resource_group_name = var.dns_zone_resource_group
 }
 
 data "azurerm_private_dns_zone" "blob_private_zone" {
-  count = var.enable_private_networks ? 1 : 0
   name                = var.blob_private_zone
   resource_group_name = var.dns_zone_resource_group
 }
 
 data "azurerm_private_dns_zone" "kv_private_dns_zone" {
-  count = var.enable_private_networks ? 1 : 0
   name                = var.kv_private_zone
   resource_group_name = var.dns_zone_resource_group
 }
 
 data "azurerm_private_dns_zone" "sql_private_dns_zone" {
-  count = var.enable_private_networks ? 1 : 0
   name                = var.sql_private_zone
   resource_group_name = var.dns_zone_resource_group
 }
 
 data "azurerm_private_dns_zone" "adb_private_dns_zone" {
-  count               = var.enable_private_networks ? 1 : 0
   name                = var.adb_private_zone
   resource_group_name = var.dns_zone_resource_group
+}
+
+# Get information about all the private endpoint connections on the storage account
+data "azapi_resource" "private_endpoint_connections" {
+  type = "Microsoft.Storage/storageAccounts@2022-09-01"
 }
--- deploy/azure/infra/locals.tf
+++ deploy/azure/infra/locals.tf
@@ -1,14 +1,13 @@
 
 locals {
 
-# Create a list of the items for which endpoints need to be created
-# This only applies if the enable_private_networks is enabled
-private_endpoint_list = var.enable_private_networks ? {
+  # Create a list of the items for which endpoints need to be created
+  # This only applies if the enable_private_networks is enabled
+  private_endpoint_list = var.enable_private_networks ? {
     blob = module.adls_default.storage_account_ids[0]
     adls = module.adls_default.storage_account_ids[1]
     kv   = module.kv_default.id
     sql  = module.sql.sql_server_id
     adb  = module.adb.adb_databricks_id
-} : {}
-
+  } : {}
 }
