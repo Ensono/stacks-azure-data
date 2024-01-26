308ae555716efc039c3afc165ea6412b7b1a73c2
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2024-01-26 16:54:14 +0000

    Removed unused variables Defaulted some to be the value of another if not set
    Signed-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- deploy/azure/infra/data.tf
+++ deploy/azure/infra/data.tf
@@ -16,30 +16,30 @@ data "azurerm_virtual_network" "vnet" {
 data "azurerm_private_dns_zone" "dfs_private_zone" {
   count               = var.enable_private_networks ? 1 : 0
   name                = var.dfs_private_zone
-  resource_group_name = var.dns_zone_resource_group
+  resource_group_name = var.dns_zone_resource_group_name
 }
 
 data "azurerm_private_dns_zone" "blob_private_zone" {
   count               = var.enable_private_networks ? 1 : 0
   name                = var.blob_private_zone
-  resource_group_name = var.dns_zone_resource_group
+  resource_group_name = var.dns_zone_resource_group_name
 }
 
 data "azurerm_private_dns_zone" "kv_private_dns_zone" {
   count               = var.enable_private_networks ? 1 : 0
   name                = var.kv_private_zone
-  resource_group_name = var.dns_zone_resource_group
+  resource_group_name = var.dns_zone_resource_group_name
 }
 
 data "azurerm_private_dns_zone" "sql_private_dns_zone" {
   count               = var.enable_private_networks ? 1 : 0
   name                = var.sql_private_zone
-  resource_group_name = var.dns_zone_resource_group
+  resource_group_name = var.dns_zone_resource_group_name
 }
 
 data "azurerm_private_dns_zone" "adb_private_dns_zone" {
   count               = var.enable_private_networks ? 1 : 0
   name                = var.adb_private_zone
-  resource_group_name = var.dns_zone_resource_group
+  resource_group_name = var.dns_zone_resource_group_name
 }
 
