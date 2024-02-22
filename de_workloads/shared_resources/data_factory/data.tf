60c100f7b74f83d4db43f1b0af26e24fd92cda9c
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2024-02-22 09:55:49 +0000

    Removed eroneous data block
    Signed-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- de_workloads/shared_resources/data_factory/data.tf
+++ de_workloads/shared_resources/data_factory/data.tf
@@ -7,13 +7,3 @@ data "azurerm_key_vault" "key_vault" {
   name                = var.key_vault_name
   resource_group_name = var.key_vault_resource_group_name
 }
-
-data "terraform_remote_state" "data_infra" {
-  backend = "azurerm"
-  config = {
-    resource_group_name  = var.tf_state_resource_group_name
-    storage_account_name = var.tf_state_storage_account_name
-    container_name       = var.tf_state_container_name
-    key                  = var.tf_state_container_key_name
-  }
-}
