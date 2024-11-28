94059f6c1789297c8f94ea183fb3f6a68a4eb5f7
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2024-11-28 14:23:06 +0000

    Updates to versions Getting public network deploying
    Signed by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- deploy/azure/infra/provider.tf
+++ deploy/azure/infra/provider.tf
@@ -1,6 +1,21 @@
 terraform {
   backend "azurerm" {
   }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.117.0"
+    }
+    databricks = {
+      source  = "databricks/databricks"
+      version = "1.59.0"
+    }
+    azapi = {
+      source  = "Azure/azapi"
+      version = "1.15.0"
+    }
+  }
 }
 
 provider "azurerm" {
