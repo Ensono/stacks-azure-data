8320c68e9635b3d5907ec10e20c919853af7950f
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2025-01-30 16:34:13 +0000

    Modified hub name so it fits in with the naming convention of everything else
    Signed by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- deploy/terraform/networking/labels.tf
+++ deploy/terraform/networking/labels.tf
@@ -13,8 +13,7 @@ module "label_default" {
 module "label_hub" {
   source          = "git::https://github.com/cloudposse/terraform-null-label.git?ref=0.24.1"
   namespace       = format("%s-%s", substr(var.name_company, 0, 16), substr(var.name_project, 0, 16))
-  stage           = "hub"
-  name            = lookup(local.location_name_map, var.resource_group_location)
+  name            = "${lookup(local.location_name_map, var.resource_group_location)}-hub"
   attributes      = var.attributes
   delimiter       = "-"
   id_length_limit = 60
