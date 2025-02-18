293fd2077578b049d3a456b88eba5a4a2fac0198
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2025-02-18 16:17:50 +0000

    Added missing file and reference to file
    Signed by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- deploy/terraform/networking/locals.tf
+++ deploy/terraform/networking/locals.tf
@@ -213,7 +213,7 @@ locals {
 
   # Create a local object for the template mapping so that the script files can be generated
   templates = flatten([
-    for file in ["envvars.bash.tpl", "envvars.ps1.tpl", "inputs.tfvars.tpl"] : [
+    for file in ["envvars.bash.tpl", "envvars.ps1.tpl", "inputs.auto.tfvars.tpl"] : [
       for name, detail in local.environments : {
         envname  = name
         file     = file
