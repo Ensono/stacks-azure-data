8434914abe29f2f9864778192dfe4eefd9ba8622
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2025-02-27 14:57:47 +0000

    Set default action to deny when using private networking
    Signed by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- deploy/terraform/infra/kv.tf
+++ deploy/terraform/infra/kv.tf
@@ -20,7 +20,7 @@ module "kv_default" {
   public_network_access_enabled = var.enable_private_networks ? var.kv_public_network_access_enabled : true # enabled if only public network otherwise cannot connect
   kv_private_dns_zone_id        = var.enable_private_networks ? tostring(data.azurerm_private_dns_zone.kv_private_dns_zone[0].id) : ""
   virtual_network_subnet_ids    = var.enable_private_networks ? [tostring(data.azurerm_subnet.pe_subnet[0].id), tostring(data.azurerm_subnet.build_agent_subnet[0].id)] : []
-  network_acl_default_action    = "Allow"
+  network_acl_default_action    = var.enable_private_networks ? "Deny" : "Allow"
   reader_object_ids             = [module.adf.adf_managed_identity]
 
   depends_on = [module.adf]
